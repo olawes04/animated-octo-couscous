b4156250b006ef2e86cb26fb56d617574207f8a2
olawes04 <144459640+olawes04@users.noreply.github.com>
2023-12-08 09:28:25 +0000

    Update Mérgé.py

--- Mérgé.py
+++ Mérgé.py
@@ -3,28 +3,22 @@ import random
 
 
 def merge(list_a, list_b):
-    i=0
-    j=0
-    merged_list=[]
-    while len(merged_list)<len(list_a)+len(list_b):
-        if list_a[i]<list_b[j]:
+    i = 0
+    j = 0
+    merged_list = []
+    while i < len(list_a) and j < len(list_b):
+        if list_a[i] < list_b[j]:
             merged_list.append(list_a[i])
-            i+=1
+            i += 1
         else:
             merged_list.append(list_b[j])
-            j+=1
-        if i==len(list_a):
-            y=j
-            for x in range(j,len(list_b)):
-                merged_list.append(list_b[y])
-                y+=1
-            break
-        elif i==len(list_b):
-            y=i
-            for x in range(i,len(list_a)):
-                merged_list.append(list_a[y])
-                y+=1
-            break
+            j += 1
+    while i < len(list_a):
+        merged_list.append(list_a[i])
+        i += 1
+    while j < len(list_b):
+        merged_list.append(list_b[j])
+        j += 1
     return merged_list
 
 #print(merge([1,2],[1,4,7]))
